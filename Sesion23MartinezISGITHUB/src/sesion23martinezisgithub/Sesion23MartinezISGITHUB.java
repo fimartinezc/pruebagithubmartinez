75984c7346a179d2307f5d9b2d2ca93ffbd51072
fimartinezc <fimartinezc@yahoo.com.mx>
2024-05-17 16:44:45 -0600

    Confirmacion Martinez 1
    OK

---
+++ Sesion23MartinezISGITHUB/src/sesion23martinezisgithub/Sesion23MartinezISGITHUB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion23martinezisgithub;
+
+/**
+ *
+ * @author fcant
+ */
+public class Sesion23MartinezISGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba de GITHUB 1 MARTINEZ");
+    }
+}
